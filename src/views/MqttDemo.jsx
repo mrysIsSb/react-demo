5d5563a7909dbd22baa8c9dda3001b7850a8c99e
mrys <yijie@yffsc.com>
2022-08-10 17:14:40 +0800

    commit

--- src/views/MqttDemo.jsx
+++ src/views/MqttDemo.jsx
@@ -1,12 +1,31 @@
-import * as echarts from 'echarts';
 import React, { useEffect, useState } from 'react';
 import mqtt from '../js/mqtt.min.js';
 
-const ms = [];
-const MqttDemo = (props) => {
-  const [client, setClient] = useState(null);
-  const [msgs, setMsgs] = useState([]);
+let ms = [];
 
+const MsgBox = ({ msgs }) => {
+  return msgs.map((msg, index) => {
+    return (
+      <p key={index}>
+        {msg}
+      </p>
+    )
+  });
+}
+
+
+const MqttDemo = (props) => {
+  console.log("a", ms)
+  const [client, setClient] = useState(null);
+  const [msgs, setMsgs] = useState(ms);
+  // console.log('MqttDemo');
+  useEffect(() => {
+    return () => {
+      console.log(msgs)
+      ms = msgs;
+      console.log("b", ms)
+    }
+  }, [msgs]);
   useEffect(() => {
     // 基于准备好的dom，初始化echarts实例
     // var myChart = echarts.init(document.getElementById('main'));
@@ -21,6 +40,7 @@ const MqttDemo = (props) => {
       var i = 100;
       client.on('message', function (topic, message) {
         setMsgs([...msgs, [topic, message].join(": ")]);
+        // msgs = [...msgs, [topic, message].join(": ")];
         console.log('msgs', msgs);
         i--
         if (i < 0) {
@@ -49,15 +69,7 @@ const MqttDemo = (props) => {
       <h1>
         hello mqtt
       </h1>
-      {
-        msgs.map((msg, index) => {
-          return (
-            <p key={index}>
-              {msg}
-            </p>
-          )
-        })
-      }
+      <MsgBox msgs={msgs} />
       <div id='main' style={
         {
           width: "50vw",
